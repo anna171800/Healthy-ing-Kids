a305a02f76765e928cca3539751b07986249931d
anna171800 <102045970+anna171800@users.noreply.github.com>
2024-05-20 02:51:56 +0900

    Update healthyingkids.py

--- healthyingkids.py
+++ healthyingkids.py
@@ -224,7 +224,7 @@ elif selection == None or selection == "육아일기":
         
         url = 'https://raw.githubusercontent.com/annayjs/healthyingkid/main/child_info.csv'  # GitHub에 있는 CSV 파일의 URL
         response = requests.get(url)
-        open('child_info.csv', 'wb').write(response.content)
+        #open('child_info.csv', 'wb').write(response.content)
 
         child_data = pd.read_csv('child_info.csv')
         child_name_list=child_data['name'].to_list()
